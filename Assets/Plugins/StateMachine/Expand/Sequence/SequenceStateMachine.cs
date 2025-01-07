13e40b37a377ff616a708857c3c01c0b9f39264a
ZYLin <jor13579@gmail.com>
2025-01-07 21:29:53 +0800

    Set IgnoreEnter to default True

--- Assets/Plugins/StateMachine/Expand/Sequence/SequenceStateMachine.cs
+++ Assets/Plugins/StateMachine/Expand/Sequence/SequenceStateMachine.cs
@@ -33,7 +33,7 @@ namespace StateMachine
             } 
         }
 
-        public bool IgnoreEnter { get; set; } = false;
+        public bool IgnoreEnter { get; set; } = true;
 
         public void OrderBy(ISequenceOrder order)
         {
